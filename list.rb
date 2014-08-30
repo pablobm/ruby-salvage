c228d45a016636ecfcdf6274d438090ca41bb8c8
Pablo Brasero <pablo@pablobm.com>
2014-08-30 11:08:48 +0100

    Avoid discouraged method and remove warning

--- list.rb
+++ list.rb
@@ -3,8 +3,7 @@ require 'salvage/server'
 Salvage::Server.new(8089)
 
 gem_versions = Hash.new{|h, k| h[k] = [] }
-gems = Gem::Specification.all
-gems.each do |gem|
+Gem::Specification.each do |gem|
   gem_versions[gem.name] << gem.version
 end
 
