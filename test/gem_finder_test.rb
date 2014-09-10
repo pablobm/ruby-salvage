36f2e19c260e5ea0a32a80dd9b4d139d8bbf30a4
Pablo Brasero <pablo@new-bamboo.co.uk>
2014-09-10 10:31:50 +0100

    Actually, I like this style better

--- test/gem_finder_test.rb
+++ test/gem_finder_test.rb
@@ -1,14 +1,14 @@
 require 'test_helper'
-require 'salvage/rvm'
+require 'salvage/gem_finder'
 
 describe Salvage::GemFinder do
 
   describe "#gem_dirs" do
-    it "lists possible gem paths under the given path" do
-      base_path = fixture_path
+    it "lists RVM gems" do
+      base_path = fixture_path('rvm')
       finder = Salvage::GemFinder.new(base_path)
-
-      expected_rvm = [
+      actual = finder.gem_dirs.sort
+      expected = [
         'ruby-1.9.3-p545',
         'ruby-2.1.2',
         'ruby-2.0.0-p353',
@@ -17,15 +17,19 @@ describe Salvage::GemFinder do
         'ruby-2.0.0-p353@global',
         'ruby-2.1.1',
         'ruby-2.1.2@global',
-      ].map{|dir| File.join(base_path, 'rvm', 'gems', dir) }
+      ].map{|dir| File.join(base_path, 'gems', dir) }.sort
 
-      expected_rbenv = [
+      assert_equal expected, actual
+    end
+
+    it "lists rbenv gems" do
+      base_path = fixture_path('rbenv')
+      finder = Salvage::GemFinder.new(base_path)
+      actual = finder.gem_dirs.sort
+      expected = [
         '1.9.3-p547/lib/ruby/gems/1.9.1',
         '2.1.2/lib/ruby/gems/2.1.0',
-      ].map{|dir| File.join(base_path, 'rbenv', 'versions', dir) }
-
-      expected = (expected_rvm + expected_rbenv).sort
-      actual = finder.gem_dirs.sort
+      ].map{|dir| File.join(base_path, 'versions', dir) }.sort
 
       assert_equal expected, actual
     end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/spec'
 
-def fixture_path
+def fixture_path(relpath)
   here = File.dirname(__FILE__)
-  File.join(here, 'fixtures')
+  File.join(here, 'fixtures', relpath)
 end
